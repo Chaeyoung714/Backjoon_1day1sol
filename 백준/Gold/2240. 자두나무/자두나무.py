ae846268ee0ac063933d86fc31385641bd62bc93
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2024-04-02 15:42:29 +0900

    [Gold V] Title: 자두나무, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.py
+++ 백준/Gold/2240. 자두나무/자두나무.py
@@ -2,37 +2,38 @@ import sys
 input = sys.stdin.readline
 
 t, w = map(int, input().split())
+tree = [0]
 
-trees = [0]
 for _ in range(t):
-    trees.append(int(input()))
+    tree.append(int(input().strip()))
 
-DP = [[0] * (w + 1) for _ in range(t + 1)]
+# tree = [0, 2, 1, 1, 2, 2, 1, 1]
 
-for i in range(t + 1):
+#초기화    
+DP = [[0 for _ in range(w+1)] for _ in range(t+1)]
 
-    #한 번도 안 움직인 상태 값(DP[*][0]) 채움 (초기화)
-    if trees[i] == 1:
-        DP[i][0] = DP[i-1][0] + 1 #i초까지 한번도 안 움직였을 때 자두 개수
-    
+for i in range(1, t+1): #자두 위치가 나무1에서 변하지 않음
+    if tree[i] == 2:
+        DP[i][0] = DP[i-1][0]
     else:
-        DP[i][0] = DP[i-1][0] #이전과 변함없는 상태
+        DP[i][0] = DP[i-1][0] + 1
 
-
-    #1번 이상 움직이는 경우에 대해 탐색
+#DP테이블 채우기
+for i in range(1, t+1):
+    target = tree[i] #자두를 먹을 수 있는 나무 위치
     for j in range(1, w+1):
-        
-        #i초에 2번나무에서 자두가 떨어짐 + 2번나무에 위치
-        if trees[i] == 2 and j % 2 == 1:
-            DP[i][j] = max(DP[i-1][j-1], DP[i-1][j]) + 1
-
-        #i초에 1번나무에서 자두가 떨어짐 + 1번나무에 위치
-        elif trees[i] == 1 and j % 2 == 0:
-            DP[i][j] = max(DP[i-1][j-1], DP[i-1][j]) + 1
-
-        #i초에 자두가 떨어지는 나무번호와 현재 나무위치가 다른 경우
+        if j % 2 == 0:
+            where = 1
         else:
+            where = 2
+        
+        #자두를 먹을 수 있는 위치와 내 현위치 비교
+        if target == where: #먹음. 따라서 DP[n] = max(DP[n-1]) + 1!
+            # 내가 where에 서 있을 수 있는 경우는 2가지 <- 직전에 이동함 or 직전에 이동하지 않음
+            
+            DP[i][j] = max(DP[i-1][j-1], DP[i-1][j]) + 1
+        
+        else: #먹을 수 없음. 따라서 DP[n] = max(DP[n-1])에서 변화 없음
             DP[i][j] = max(DP[i-1][j-1], DP[i-1][j])
-
-#마지막 행 (마지막까지 자두를 다 받음) 에서의 최대값 
+        
 print(max(DP[t]))
