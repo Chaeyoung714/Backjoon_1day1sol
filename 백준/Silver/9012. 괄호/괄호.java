799832a9f20d43739a6d9e5dd03fcde583350776
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-03-27 11:19:52 +0900

    [Silver IV] Title: 괄호, Time: 208 ms, Memory: 18368 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,44 @@
+import java.util.EmptyStackException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int repeatAmount = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < repeatAmount; i++) {
+            String line = sc.nextLine();
+            if (isVPS(line)) {
+                System.out.println("YES");
+            } else  {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    private static boolean isVPS(String line) {
+        Stack<String> stack = new Stack<>();
+
+        String[] lines = line.split("");
+
+        for (String gualho : lines) {
+            if (gualho.equals("(")) {
+                stack.push(gualho);
+            }
+            if (gualho.equals(")")) {
+                try {
+                    String pop = stack.pop();
+                    if (pop.equals("(")) {
+                        continue;
+                    }
+                } catch (EmptyStackException e) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+}
