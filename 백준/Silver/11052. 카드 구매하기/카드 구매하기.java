166521ba469ed0e9f816048c4ec8e6dcf15f0057
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-05-08 11:09:50 +0900

    [Silver I] Title: 카드 구매하기, Time: 240 ms, Memory: 21144 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static int[] DP;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cardCount = sc.nextInt();
+        int[] cardPackFees = new int[cardCount + 1];
+        DP = new int[cardCount + 1];
+
+        for (int i = 1; i <= cardCount; i++) {
+            cardPackFees[i] = sc.nextInt();
+        }
+
+        int maxFee = calculateMaxFee(cardPackFees);
+        System.out.println(maxFee);
+    }
+
+    private static int calculateMaxFee(int[] cards) {
+        DP[1] = cards[1];
+
+        int cardCount = cards.length - 1;
+        if (cardCount == 1) {
+            return DP[1];
+        }
+
+        for (int count = 2; count <= cardCount; count++) {
+            int maxFeeByCount = 0;
+            for (int i = count; i >= 1; i--) {
+                int currFee = DP[count - i] + cards[i];
+                maxFeeByCount = Math.max(maxFeeByCount, currFee);
+            }
+            DP[count] = maxFeeByCount;
+        }
+
+        return DP[cardCount];
+    }
+}
