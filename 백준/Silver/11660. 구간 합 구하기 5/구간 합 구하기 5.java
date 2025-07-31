14ad389c53ff5002af952a5af8d55b0593225d3a
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-07-31 11:04:54 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1312 ms, Memory: 128556 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -5,43 +5,69 @@ import java.util.StringTokenizer;
 
 public class Main {
     /**
-     * sol1: O(n^3) -> 시간초과
-     * sol2: 구간합 ->
+     * sol1: O(n^3) -> 시간초과 sol2: 구간합 -> DP 사용하여 값 누적
      */
+
+    static int[][] table;
+    static int[][] DP;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] map = new int[N+1][N+1];
-        int[][] sum = new int[N+1][N+1];
+        table = new int[N + 1][N + 1];
+        DP = new int[N + 1][N + 1];
 
-        for (int i=1; i<N+1; i++) {
+        for (int i = 1; i < N + 1; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=1; j<N+1; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j < N + 1; j++) {
+                table[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 누적합 배열 만들기
-        for (int i=1; i<N+1; i++) {
-            for (int j=1; j<N+1; j++) {
-                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + map[i][j];
-            }
-        }
+        fillDP(N);
 
-        for (int i=1; i<=M; i++) {
+        int[] results = new int[M];
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
+            results[i] = calculateSum(x1, y1, x2, y2);
+        }
 
-            int result = sum[x2][y2] - sum[x2][y1-1] - sum[x1-1][y2] + sum[x1-1][y1-1];
+        for (int result : results) {
             System.out.println(result);
         }
 
         br.close();
+    }
+
+    private static void fillDP(int N) {
+        // 누적합 배열 만들기
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j < N + 1; j++) {
+                if (i == 1 && j == 1) {
+                    DP[i][j] = table[i][j];
+                    continue;
+                }
+                if (i == 1) {
+                    DP[i][j] = DP[i][j - 1] + table[i][j];
+                    continue;
+                }
+                if (j == 1) {
+                    DP[i][j] = DP[i - 1][j] + table[i][j];
+                    continue;
+                }
+                DP[i][j] = DP[i - 1][j] + DP[i][j - 1] - DP[i - 1][j - 1] + table[i][j];
+            }
+        }
+    }
+
+    private static int calculateSum(int x1, int y1, int x2, int y2) {
+        return DP[x2][y2] - (DP[x2][y1 - 1] + DP[x1 - 1][y2] - DP[x1 - 1][y1 - 1]);
     }
 }
