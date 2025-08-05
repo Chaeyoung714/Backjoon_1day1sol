70400f23880b0658a1a105a7cdcb55717ee06fcd
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-08-05 21:42:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1128 ms, Memory: 59804 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,27 +1,40 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+    static long[] sums;
+
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       String[] s1 = br.readLine().split(" ");
-        
-        int N = Integer.parseInt(s1[0]);
-        int M = Integer.parseInt(s1[1]);
-    
-        int[] sum = new int[N+1];
-        
-        String[] s2 = br.readLine().split(" ");
-        sum[0] = 0;
-        for(int i = 1; i <= N; i ++) {
-            sum[i] = sum[i-1] + Integer.parseInt(s2[i-1]);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        sums = new long[n + 1];
+
+        st = new StringTokenizer(bufferedReader.readLine());
+        sums[1] = Integer.parseInt(st.nextToken());
+        for (int i = 2; i <= n; i++) {
+            sums[i] = Integer.parseInt(st.nextToken()) + sums[i - 1];
         }
-        
-        for(int i = 0; i < M; i++) {
-            String[] s3 = br.readLine().split(" ");
-            int start = Integer.parseInt(s3[0]);
-            int end = Integer.parseInt(s3[1]);
-            System.out.println(sum[end] - sum[start-1]);
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(bufferedReader.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            long sum = countSum(start, end);
+            System.out.println(sum);
         }
+    }
+
+    private static long countSum(int start, int end) {
+        if (start == 1) {
+            return sums[end];
+        }
+        return sums[end] - sums[start - 1];
     }
 }
