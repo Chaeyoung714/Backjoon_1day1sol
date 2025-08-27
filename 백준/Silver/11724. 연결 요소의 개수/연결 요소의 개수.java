abd44961d63af41965880309c76964d7d6f707b2
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-08-28 01:41:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 460 ms, Memory: 116640 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,47 +1,55 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 300,000,000, M <= 500,000
+ * O(n^2) -> 안됨
+ * O(nlogn) = 500,000 * 3.3 * 6 ~= 10,000,000 -> 될 듯
+ */
 public class Main {
-    
-    static int N,M;
-    static int arr[][];
-    static boolean visit[];
-    static int result=0;
-    static int x,y; 
-    
-    public static void main(String[] args)throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+    static int[] visited;
+    static int n;
+    static int m;
+    static int[][] lines;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
-        
-        arr = new int[N+1][N+1];
-        visit = new boolean[N+1];
-        
-        for(int i = 1;i < M+1;i++){
-                st = new StringTokenizer(br.readLine());
-                x=Integer.parseInt(st.nextToken());
-                y=Integer.parseInt(st.nextToken());
-                arr[x][y] = arr[y][x] = 1;
+
+        n = Integer.parseInt(st.nextToken()); // 정점 개수
+        m = Integer.parseInt(st.nextToken()); // 간선 개수
+
+        lines = new int[n + 1][n + 1]; // 간선 대신 2차원 배열로!
+        visited = new int[n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            lines[from][to] = 1;
+            lines[to][from] = 1;
         }
-        
-        for(int i = 1;i < N+1; i++) {
-			if(!visit[i]){
-				dfs(i);
+
+        int result = 0;
+        for (int edge = 1; edge <= n; edge++) {
+            if (visited[edge] == 0) {
+                dfs(edge);
                 result++;
-			}
-		}
-        System.out.println(result);
-    }
-    
-    public static void dfs(int node){
-        visit[node]=true;
-        
-        for(int i = 1;i < N+1;i++){
-            if(!visit[i] && arr[node][i]==1){
-                dfs(i);
             }
         }
-            
+
+        System.out.println(result);
+    }
+
+    private static void dfs(int from) {
+        visited[from] = 1;
+
+        for (int to = 1; to <= n; to++) {
+            if (visited[to] == 0 && lines[from][to] == 1) {
+                dfs(to);
+            }
+        }
     }
 }
