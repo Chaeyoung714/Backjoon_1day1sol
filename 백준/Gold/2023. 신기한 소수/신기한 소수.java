80c49092274f72c2343e7901e50327f58c3ff3fa
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-08-28 09:15:09 +0900

    [Gold V] Title: 신기한 소수, Time: 128 ms, Memory: 15996 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,34 +1,65 @@
+import java.io.*;
 import java.util.*;
- 
-public class Main {    
-    
-    static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int n = scan.nextInt();
-        dfs(0, "", n);
-        System.out.println(sb.toString());
-    }    
-    
-    public static void dfs(int idx, String num, int n) {
-        if(idx == n) {
-            sb.append(num + "\n");
+import java.util.stream.Collectors; //왜 import가 안됐지?
+
+/**
+ * 200,000,000, N <= 8자리
+ * N이 시간복잡도의 기준이 되지 않음ㅁ
+ * -> 10 * 100* 1000 * ... * 100,000,000
+ * -> 10 ^ 35 -> x
+ * 이때 한자리수 소수는 1 2 3 5 7 -> 5
+ */
+public class Main {
+
+    static int n;
+    static List<Integer> primes;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        primes = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            if (isPrime(i)) { //2, 3, 5, 7
+                dfs(i, 1);
+            }
+        }
+
+        List<Integer> sortedPrimes = primes.stream().sorted().collect(Collectors.toList());
+
+        StringBuilder sb = new StringBuilder();
+        for (int prime : primes) {
+            sb.append(prime + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs(int currentNum, int length) {
+        if (length >= n) {
+            primes.add(currentNum);
             return;
         }
-        
-        for(int i = 1; i <= 9; i++) {
-            if(isPrime(Integer.valueOf(num + i))) dfs(idx + 1, num + i, n);
+
+        for (int num = 1; num <= 9; num++) {
+            int nextNum = currentNum * 10 + num;
+            if (isPrime(nextNum)) {
+                dfs(nextNum, length + 1);
+            }
         }
     }
-    
-    public static boolean isPrime(int num) {
-        if(num == 1) return false;
- 
+
+    private static boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+
         int sqrt = (int) Math.sqrt(num);
-        for(int i = 2; i <= sqrt; i++) {
-            if(num % i == 0) return false;
+
+        for (int i = 2; i <= sqrt; i++) {
+            if (num % i == 0) {
+                return false;
+            }
         }
         return true;
     }
