05dec1f3ccde4df537b231ab62dee5670c2b79f0
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-09-23 02:49:12 +0900

    [Silver IV] Title: 베스트셀러, Time: 108 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int max=0;
+        Map<String, Integer> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            String str=br.readLine();
+            map.put(str,map.getOrDefault(str,0)+1);
+            max = Math.max(max,map.get(str));
+        }
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            if(entry.getValue()==max) {
+                list.add(entry.getKey());
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list.get(0));
+    }
+}
