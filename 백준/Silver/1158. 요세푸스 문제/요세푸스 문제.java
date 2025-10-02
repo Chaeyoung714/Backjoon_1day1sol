301e165afdc8450e5b2d5ca6ed4cbd0275dfbf1b
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-10-03 00:19:52 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 120 ms, Memory: 14976 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1;i <= n; i++){
+            list.add(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        int idx = 0;
+        while(list.size() > 1) {
+            idx = (idx + (k - 1)) % list.size();
+            sb.append(list.remove(idx) + ", ");
+        }
+        sb.append(list.get(0)+">");
+
+        System.out.println(sb.toString());
+    }
+
+}
