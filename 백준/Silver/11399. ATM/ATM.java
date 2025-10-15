68bbd9008489f8fa958b160159508d8ee831ddaf
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-10-16 02:30:12 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,47 +1,23 @@
-import java.util.*;
 import java.io.*;
-
-/**
- * 100,000,000번, N <= 1,000
- * O(n^2)까지 가능~! -> 어떤 정렬이라도 사용 가능
- */
+import java.util.*;
 public class Main {
-   
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
-        int[] times = new int[n];
-        String[] timeInputs = br.readLine().split(" ");
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // Pi 값들 입력 받기
         for (int i = 0; i < n; i++) {
-            times[i] = Integer.parseInt(timeInputs[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i = 1; i < n; i++) {
-            int current = i;
-            int target = i - 1;
-            while (target >= 0) {
-                if (times[target] <= times[current]) {
-                    current--;
-                    target--;
-                    break;
-                }
-
-                int tmp = times[target];
-                times[target] = times[current];
-                times[current] = tmp;
-
-                current--;
-                target--;
-            }
-        }
-
-        int sum = 0;
+        //배열을 오름차순으로 정렬
+        Arrays.sort(arr);
+        int sum = 0;  //총 합
+        int add = 0;  //더할 수
         for (int i = 0; i < n; i++) {
-            sum += times[i] * (n - i);
+            add += arr[i];
+            sum += add;
         }
-
-        System.out.println(sum);
+        System.out.print(sum);
     }
 }
